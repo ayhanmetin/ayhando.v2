0931a80058d35278d36744efa968dcdbaf77fe48
Ayhan Metin <108268012+ayhanmetin@users.noreply.github.com>
2024-02-12 13:29:43 +0100

    update

--- src/components/Nav2.jsx
+++ src/components/Nav2.jsx
@@ -1,38 +1,38 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import './CustomNavLink.css'; // Import custom CSS for hover effect
 
 export default function Nav2() {
   return (
-    <>
-      <div className='container'>
-        <nav className='nav nav-underline justify-content-between mt-3 ps-5 pb-2'>
-          <NavLink
-            className='nav-item nav-link link-body-emphasis'
-            to='https://www.ayhanmetin.eu/practice'
+    <div className='container'>
+      <nav className='nav justify-content-between mt-3 ps-5 pb-2'>
+        <NavLink
+          target='_blank'
+          className='nav-item nav-link custom-nav-link'
+          to='https://www.ayhanmetin.eu/practice'
+        >
+          Testing Practice{' '}
+          <svg
+            viewBox='0 0 24 24'
+            width='1em'
+            height='1em'
+            fill='currentColor'
+            focusable='false'
+            aria-hidden='true'
           >
-            Testing Practice{' '}
-            <svg
-              viewBox='0 0 24 24'
-              width='1em'
-              height='1em'
-              fill='currentColor'
-              focusable='false'
-              aria-hidden='true'
-            >
-              <path d='M.443 12c0 .414.336.75.75.75h20.869l-7.72 7.72a.75.75 0 0 0 1.06 1.06l7.94-7.94a2.252 2.252 0 0 0 0-3.18l-7.94-7.94a.75.75 0 0 0-1.06 1.06l7.72 7.72H1.192a.75.75 0 0 0-.75.75z'></path>
-            </svg>
-          </NavLink>
-          <NavLink className='nav-item nav-link link-body-emphasis' to='/'>
-            Cypress
-          </NavLink>
-          <NavLink className='nav-item nav-link link-body-emphasis' to='/'>
-            Selenium
-          </NavLink>
-          <NavLink className='nav-item nav-link link-body-emphasis' to='/'>
-            Playwright
-          </NavLink>
-        </nav>
-      </div>
-    </>
+            <path d='M.443 12c0 .414.336.75.75.75h20.869l-7.72 7.72a.75.75 0 0 0 1.06 1.06l7.94-7.94a2.252 2.252 0 0 0 0-3.18l-7.94-7.94a.75.75 0 0 0-1.06 1.06l7.72 7.72H1.192a.75.75 0 0 0-.75.75z'></path>
+          </svg>
+        </NavLink>
+        <NavLink className='nav-item nav-link custom-nav-link' to='/'>
+          Cypress
+        </NavLink>
+        <NavLink className='nav-item nav-link custom-nav-link' to='/'>
+          Selenium
+        </NavLink>
+        <NavLink className='nav-item nav-link custom-nav-link' to='/'>
+          Playwright
+        </NavLink>
+      </nav>
+    </div>
   );
 }
